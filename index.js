2a0061c64f9c98f09e214d6a0e09972c6a7f00b5
unknown <teraco@gmail.com>
2018-12-08 14:00:18 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-console */
+const delay = func => new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(func());
+  }, 1000);
+});
+
+console.log('a');
+delay(() => {
+  console.log('c');
+}).then(() => {
+  return delay(() => console.log('d'));
+}).then(() => {
+  return delay(() => console.log('e'));
+});
+console.log('b');
+
+// second
