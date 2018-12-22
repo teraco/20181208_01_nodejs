01ff2ccc28761e5818accf0245f8a14bf1154214
unknown <teraco@gmail.com>
2018-12-22 14:53:57 +0900

    sendmail and commit cosmosdb

--- httprequest.js
+++ httprequest.js
@@ -8,18 +8,23 @@ const EMAIL = process.env.MY_EMAIL;
 
 const CosmosClient = require('@azure/cosmos').CosmosClient;
 
-const config = require('./config');
 const url = require('url');
 
-const endpoint = config.endpoint;
-const masterKey = config.primaryKey;
+const endpoint = process.env.NODEJS_endpoint;
+const masterKey = process.env.NODEJS_primaryKey;
 
 const client = new CosmosClient({ endpoint: endpoint, auth: { masterKey: masterKey } });
 
 const HttpStatusCodes = { NOTFOUND: 404 };
 
-const databaseId = config.database.id;
-const containerId = config.container.id;
+const databaseId = process.env.NODEJS_database_id;
+const containerId = process.env.NODEJS_container_id;
+
+// 日付計算
+const now = new Date();
+const datetime = (now.getFullYear() + "-"  + (now.getMonth() + 1) + "-" + now.getDate() + "-" + now.getHours() + "-" + now.getMinutes());
+const lastdate = new Date(now.getFullYear(), (now.getMonth() + 1) ,0)
+const remaindate = (lastdate.getDate() - now.getDate() + 1)
 
 // using SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
@@ -63,111 +68,70 @@ sgMail.setApiKey(SENDGRID_API_KEY);
 
   // 要素取得
   await page.waitForSelector('.data2')
-  const datavolume = await page.evaluate((selector) => {
+  const datavolumestr = await page.evaluate((selector) => {
     return document.querySelector(selector).innerText;
   }, '.data2')
 
-  console.log(datavolume);
+  console.log(datavolumestr);
   
   // ブラウザの終了.
   await browser.close();
 
-})(); // 最後の()は定義した関数の即時実行
+  // 容量計算処理
+  const datavolume = (datavolumestr.slice(0, datavolumestr.length - 2))
+  const remaindatavolume = datavolume - 12000
+  const remaindatavolumeperday = (remaindatavolume / remaindate)
+  console.log(remaindatavolume)
+  console.log(remaindatavolumeperday)
 
-/*
-const msg = {
-  to: EMAIL,
-  from: EMAIL,
-  subject: 'Sending with SendGrid is Fun(APIKEY)',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  // DB用JSON作成
+  const itembodyjson = {
+    "myitem": {
+        "id": `${datetime}`,
+        "remaindatavolume": `${remaindatavolume}`,
+        "remaindatavolumeperday": `${remaindatavolumeperday}`
+  }
 };
-sgMail.send(msg);
-*/
 
+  // メール送信
+  const msg = {
+    to: EMAIL,
+    from: EMAIL,
+    subject: `[IIJ_datavolume]: ${datetime} / ${remaindatavolume}MB`,
+    html: `[IIJ_datavolume]<br>${datetime}<br>${remaindatavolume}MB<br>${remaindatavolumeperday}MB`,
+  };
+  sgMail.send(msg);
 
-// Create the database if it does not exist
-async function createDatabase() {
-  const { database } = await client.databases.createIfNotExists({ id: databaseId });
-  console.log(`Created database:\n${database.id}\n`);
-}
-
-// Read the database definition
-async function readDatabase() {
-  const { body: databaseDefinition } = await client.database(databaseId).read();
-  console.log(`Reading database:\n${databaseDefinition.id}\n`);
-}
-
-// Create the container if it does not exist
-async function createContainer() {
-  const { container } = await client.database(databaseId).containers.createIfNotExists({ id: containerId });
-  console.log(`Created container:\n${config.container.id}\n`);
-}
-
-// Read the container definition
-async function readContainer() {
-  const { body: containerDefinition } = await client.database(databaseId).container(containerId).read();
-  console.log(`Reading container:\n${containerDefinition.id}\n`);
-}
-
-// Create family item if it does not exist
-async function createFamilyItem(itemBody) {
+  // Create data if it does not exist
+  async function createFamilyItem(itemBody) {
     try {
         // read the item to see if it exists
         const { item } = await client.database(databaseId).container(containerId).item(itemBody.id).read();
         console.log(`Item with family id ${itemBody.id} already exists\n`);
     }
     catch (error) {
-       // create the family item if it does not exist
-       if (error.code === HttpStatusCodes.NOTFOUND) {
-           const { item } = await client.database(databaseId).container(containerId).items.create(itemBody);
-           console.log(`Created family item with id:\n${itemBody.id}\n`);
-       } else {
-           throw error;
-       }
+      // create the data if it does not exist
+      if (error.code === HttpStatusCodes.NOTFOUND) {
+          const { item } = await client.database(databaseId).container(containerId).items.create(itemBody);
+          console.log(`Created data with id:\n${itemBody.id}\n`);
+      } else {
+          throw error;
+      }
     }
   };
 
-// Query the container using SQL
-async function queryContainer() {
-    console.log(`Querying container:\n${config.container.id}`);
-  
-    // query to return all children in a family
-    const querySpec = {
-       query: "SELECT VALUE r.children FROM root r WHERE r.lastName = @lastName",
-       parameters: [
-           {
-               name: "@lastName",
-               value: "Andersen"
-           }
-       ]
-   };
-  
-   const { result: results } = await client.database(databaseId).container(containerId).items.query(querySpec).toArray();
-   for (var queryResult of results) {
-       let resultString = JSON.stringify(queryResult);
-       console.log(`\tQuery returned ${resultString}\n`);
-   }
-  };
-
-// Exit the app with a prompt
-// @param {message} message - The message to display
-function exit(message) {
+  // Exit the app with a prompt
+  // @param {message} message - The message to display
+  function exit(message) {
   console.log(message);
-  console.log('Press any key to exit');
-  process.stdin.setRawMode(true);
-  process.stdin.resume();
-  process.stdin.on('data', process.exit.bind(process, 0));
-}
+  // console.log('Press any key to exit');
+  // process.stdin.setRawMode(true);
+  // process.stdin.resume();
+  // process.stdin.on('data', process.exit.bind(process, 0));
+  }
 
-createDatabase()
-  .then(() => readDatabase())
-  .then(() => createContainer())
-  .then(() => readContainer())
-  .then(() => createFamilyItem(config.items.Andersen))
-  .then(() => createFamilyItem(config.items.Wakefield))
-  .then(() => createFamilyItem(config.items.iijmio))
-  .then(() => queryContainer())
+  createFamilyItem(itembodyjson.myitem)
+    .then(() => { exit(`Completed successfully`); })
+    .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
 
-  .then(() => { exit(`Completed successfully`); })
-  .catch((error) => { exit(`Completed with error ${JSON.stringify(error)}`) });
+})(); // 最後の()は定義した関数の即時実行
